d63a31dd92112c924fcfe1695924e6b63e1d9703
oscarel <oscar.kemuel5@gmail.com>
2020-07-29 18:34:57 -0300

    Updates

---
+++ precache-manifest.8545a3de9813f003b68b872ee3ddd9ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160ebf7efbb8b0128c5a40a4abe6ae27",
+    "url": "/github-repos/index.html"
+  },
+  {
+    "revision": "cad42925b9fb712838f0",
+    "url": "/github-repos/static/js/2.1fbd7f61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-repos/static/js/2.1fbd7f61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a4a3f1588e5ba9b84a0",
+    "url": "/github-repos/static/js/main.dfaa2e78.chunk.js"
+  },
+  {
+    "revision": "ff2d528dcc3f4a32ea59",
+    "url": "/github-repos/static/js/runtime-main.e6ba593f.js"
+  }
+]);
